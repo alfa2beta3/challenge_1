f76e55ff564669489630c7bdf12a7f0ac1415b3f
alfa2beta3 <leexingyang180@gmail.com>
2023-08-10 15:20:59 +0800

    Implement the intergration test of test_update_grid(), fixed small mistake on game.py

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -11,14 +11,41 @@ class TestGrid(TestCase):
         # |_|_|_|
         # |_|_|_|
 
-        test_grid.flip(2, 1)
-        # this flips the cell at column 2, row 1, so the grid would look like:
+        test_grid.flip(0, 0)
+        test_grid.flip(0, 1)
+        test_grid.flip(0, 2)
+        # this flips the cell at column 0, row 0, so the grid would look like:
+        # |x|_|_|
+        # |x|_|_|
+        # |x|_|_|
+
+        self.assertTrue(test_grid.get_cell(0, 0).active)
+        self.assertTrue(test_grid.get_cell(0, 1).active)
+        self.assertTrue(test_grid.get_cell(0, 2).active)
+        self.assertFalse(test_grid.get_cell(1, 0).active)
+        self.assertFalse(test_grid.get_cell(1, 1).active)
+        self.assertFalse(test_grid.get_cell(1, 2).active)
+        self.assertFalse(test_grid.get_cell(2, 0).active)
+        self.assertFalse(test_grid.get_cell(2, 1).active)
+        self.assertFalse(test_grid.get_cell(2, 2).active)
+
+        test_grid.update()
+
+        # what do we want to see after flip
         # |_|_|_|
-        # |_|_|X|
+        # |x|_|_|
         # |_|_|_|
 
         # You can get a specific cell from the grid by using the get_cell method
-        self.assertTrue(test_grid.get_cell(2, 1).active)
+        self.assertFalse(test_grid.get_cell(0, 0).active)
+        self.assertTrue(test_grid.get_cell(0, 1).active)
+        self.assertFalse(test_grid.get_cell(0, 2).active)
+        self.assertFalse(test_grid.get_cell(1, 0).active)
+        self.assertTrue(test_grid.get_cell(1, 1).active)
+        self.assertFalse(test_grid.get_cell(1, 2).active)
+        self.assertFalse(test_grid.get_cell(2, 0).active)
+        self.assertFalse(test_grid.get_cell(2, 1).active)
+        self.assertFalse(test_grid.get_cell(2, 2).active)
 
         # You should test a few scenarios of the update method here.
         # recall that the idea of the update method is to:
