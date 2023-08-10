afef3ace05502955abe918b5fe3b355b1dbf709a
alfa2beta3 <leexingyang180@gmail.com>
2023-08-10 14:24:44 +0800

    Correct the naming error of test_flip() and set the assert messages for test_update() and test_flip()

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -33,27 +33,31 @@ class TestCell(TestCase):
 
         # Scenario 1: the inactive cell is updated to be active
         inactive_cell = Cell((0, 0), (0, 0))
-        self.assertFalse(inactive_cell.active)
+        self.assertFalse(inactive_cell.active, 'the cell should be inactive initially')
 
         inactive_cell.future_state = True
         inactive_cell.update()
-        self.assertTrue(inactive_cell.active)
+        self.assertTrue(inactive_cell.active, 'future state was set to true so the cell should become active')
+        self.assertIs(inactive_cell.future_state, None, 'future state should be reset after the cell has been updated')
 
         # Scenario 2: the active cell is updated to be inactive
         active_cell = Cell((0, 0), (0, 0), True)
-        self.assertTrue(active_cell.active)
+        self.assertTrue(active_cell.active, 'the cell should be active initially')
 
         active_cell.future_state = False
         active_cell.update()
-        self.assertFalse(active_cell.active)
+        self.assertFalse(active_cell.active, 'future state was set to false so the cell should become inactive')
+        self.assertIs(inactive_cell.future_state, None, 'future state should be reset after the cell has been updated')
 
     def test_flip(self):
         # test the flip method of the cell class
 
-        active_cell = Cell((0, 0), (0, 0), active=False)
-        active_cell.flip()
-        self.assertTrue(active_cell.active)
-
-        inactive_cell = Cell((0, 0), (0, 0), active=True)
+        # Scenario 1: set the inactive cell active
+        inactive_cell = Cell((0, 0), (0, 0), active=False)
         inactive_cell.flip()
-        self.assertFalse(inactive_cell.active)
+        self.assertTrue(inactive_cell.active, 'the inactive cell is flip, so cell should become active')
+
+        # Scenario 2: set the active cell inactive
+        active_cell = Cell((0, 0), (0, 0), active=True)
+        active_cell.flip()
+        self.assertFalse(active_cell.active, 'the active cell is flip, so cell should become inactive')
